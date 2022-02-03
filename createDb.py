8e8ae679aa6d44c0e963ca29057a3d71e930feec
Juste Astrauskaite <juste.astrauskaite@gmail.com>
2022-02-03 20:28:38 +0100

    Created database with wine_list table in it. Changed the column names in csv file with Pandas. Imported the data from csv file to the table.

---
+++ createDb.py
@@ -0,0 +1,24 @@
+import csv, sqlite3
+
+conn = sqlite3.connect('wines.db') # creates a database
+cursor = conn.cursor() # method that iterrates through our db
+
+# create a table
+cursor.execute("""CREATE TABLE wine_list (
+    Name text,
+    Producer text,
+    Origin text,
+    Vegan text
+)""")
+
+with open('barnivore_new.csv', 'r') as fin:
+    dr = csv.DictReader(fin)
+    to_db = [(i['Name'], i['Producer'], i['Origin'], i['Vegan']) for i in dr]
+
+cursor.executemany('INSERT INTO wine_list (Name, Producer, Origin, Vegan) VALUES (?, ?, ?, ?);', to_db)
+
+# # commit  our command
+conn.commit()
+
+# # close our connection
+conn.close()
